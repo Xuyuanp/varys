77a7369a5691704c026248492120e944bbb0197d
Xuyuan Pang <xuyuanp@gmail.com>
2015-09-21 17:48:10 +0800

    New timeout

--- fetcher.go
+++ fetcher.go
@@ -39,13 +39,18 @@ func NewFetcher(opts FetcherOptions) Fetcher {
 
 // Fetch web page from url.
 func (f *URLFetcher) Fetch(url string) (body []byte, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), f.options.Timeout)
+	defer cancel()
+	return f.fetch(ctx, url)
+}
+
+func (f *URLFetcher) fetch(ctx context.Context, url string) ([]byte, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 	f.prepare(req)
-	ctx, cancel := f.newContext(context.Background())
-	defer cancel()
+
 	resp, err := ctxhttp.Do(ctx, f.client, req)
 	if err != nil {
 		return nil, err
@@ -61,11 +66,4 @@ func (f *URLFetcher) prepare(req *http.Request) {
 	if f.options.Prepare != nil {
 		f.options.Prepare(req)
 	}
-}
-
-func (f *URLFetcher) newContext(parent context.Context) (context.Context, context.CancelFunc) {
-	if f.options.Timeout > 0 {
-		return context.WithTimeout(parent, f.options.Timeout)
-	}
-	return context.WithCancel(parent)
 }
